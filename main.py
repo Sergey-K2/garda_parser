85b4f48a6bb4f9085df3fe6ca4220ab6da0e2766
Sergey Kozlov <serand.kozlov@gmail.com>
2024-02-27 16:30:13 +0300

    PEP corrections

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 """Основной модуль парсера."""
+
 import json
 import socket
 
 SERVER_ADDRESS = "194.87.46.252"
 SERVER_PORT = 8000
 USERNAME = "Sergey Kozlov"
-SUCCESS_PHRASE = "Nice, you're smart boy!"
+SUCCESS_PHRASE = b"\nNice, you're smart boy!\n\n".decode("utf-8")
 
 
 def parse():
@@ -20,11 +21,19 @@ def parse():
         string = s.recv(1024)
         print(string)
         structured_data = {
-            string[0:5].decode("utf-8").lstrip("0"): {
-                string[28:44].strip().decode("utf-8"): {
-                    "date": (string[129:131].decode("utf-8") + "." +
-                             string[126:128].decode("utf-8") + "." +
-                             string[121:125].decode("utf-8")),
+            string[0:5]
+            .decode("utf-8")
+            .lstrip("0"): {
+                string[28:44]
+                .strip()
+                .decode("utf-8"): {
+                    "date": (
+                        string[129:131].decode("utf-8")
+                        + "."
+                        + string[126:128].decode("utf-8")
+                        + "."
+                        + string[121:125].decode("utf-8")
+                    ),
                     "duration":
                         string[142:151].decode("utf-8").lstrip("0") + " sec",
                     "name_from": string[45:64].strip().decode("utf-8"),
@@ -32,16 +41,22 @@ def parse():
                     "numb_from": string[5:16].strip().decode("utf-8"),
                     "numb_to": string[84:94].strip().decode("utf-8"),
                     "time":
-                    string[132:140].strip().decode("utf-8").replace(':', '-')
+                    string[132:140].strip().decode("utf-8").replace(":", "-"),
                 }
-                }
+            }
         }
-        if structured_data[string[0:5].decode("utf-8").lstrip("0")][string[28:44].strip().decode("utf-8")]["duration"] == " sec":
-            structured_data[string[0:5].decode("utf-8").lstrip("0")][string[28:44].strip().decode("utf-8")]["duration"] = "0 sec"
+        if (
+            structured_data[string[0:5].decode("utf-8").lstrip("0")][
+                string[28:44].strip().decode("utf-8")
+            ]["duration"]
+            == " sec"
+        ):
+            structured_data[string[0:5].decode("utf-8").lstrip("0")][
+                string[28:44].strip().decode("utf-8")
+            ]["duration"] = "0 sec"
 
-        print(json.dumps(structured_data, indent=4).encode())
         s.send(json.dumps(structured_data, indent=4).encode())
-        response = s.recv(1028)
+        response = s.recv(1024)
         print(response)
     s.close()
 
